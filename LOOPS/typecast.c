52a5ddf4ccb397005105019c41f39742e1a885ff
shrimali dev <shrimalidev27@gmail.com>
2025-09-11 15:39:20 +0530

    Add files via upload

---
+++ LOOPS/typecast.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    int x = 69;
+    char ch = (int)x;
+    printf("%c = %d\n",ch,x);   
+   
+   
+    char h = 'E';
+   int a= (char)h;
+   printf("%d = %c",a,h);   
+   
+   return 0;
+
+}
---
+++ LOOPS/whileLOOP.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    int i=1;
+    while (i<=100) // condition lkhi hoti hai , jab hame jyada chizze na pata ho
+    {
+      if(i%2!=0)
+      printf("%d",i);
+        // printf("%d  " , i);
+        i++;
+    }
+    
+    
+
+    return 0;
+}
